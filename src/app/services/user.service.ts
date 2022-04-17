f2e00df7ea332c1284d62a9c46ddc733cc236223
Jinhwa <jinhwa014@revature.net>
2022-04-16 23:05:17 -0500

    user service added

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+}
+
+
+// this is User Service 
+// It will include 1. Login component 2. Create Account component
