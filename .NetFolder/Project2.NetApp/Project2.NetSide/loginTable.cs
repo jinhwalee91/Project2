e4d59f7e9bd3ef7c6eaff9926817b967771ae821
Shane <slegran8@uncc.edu>
2022-04-18 14:59:13 -0400

    .Net Added

---
+++ .NetFolder/Project2.NetApp/Project2.NetSide/loginTable.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project2.NetSide
+{
+    public class loginTable
+    {
+        public int userId {  get; set; }
+
+        [StringLength(30)]
+        public int username {  get; set; }
+
+        [StringLength(30)]
+        public int userEmail { get; set; }
+
+        [StringLength(30)]
+        public int userPassword { get; set; }
+    }
+}
