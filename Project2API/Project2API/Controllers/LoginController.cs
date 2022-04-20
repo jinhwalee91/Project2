1a1395b3d2571e4294f4b535373b69afd34b308a
Shane <slegran8@uncc.edu>
2022-04-20 16:53:57 -0400

    Added View Login API

---
+++ Project2API/Project2API/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project2API.Models.EF;
+
+namespace Project2API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        toptypersDBContext dbContext = new toptypersDBContext();
+
+        [HttpGet]
+        [Route("elist")]
+        public IActionResult GetLoginList()
+        {
+            var empList = from e in dbContext.LoginTables
+                          select e;
+            return Ok(empList);
+        }
+    }
+}
