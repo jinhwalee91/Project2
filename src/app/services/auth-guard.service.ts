54acfe7a6e52a43b31ec32d9b29dd6c3adfa6c83
Jinhwa <jinhwa014@revature.net>
2022-04-22 15:16:07 -0500

    login method

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate{
+
+
+
+  private _http : HttpClient; 
+  userDetail : any = "blank"
+
+  constructor(private _httpRef : HttpClient) {
+    this._http = _httpRef; 
+  }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean{
+      if(this.userDetail != "blank")
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+  }
+
+
+
+  userLogin (email : string , password : any) {
+ 
+    let url = "https://localhost:7274/api/Login/Login/"+email+'/'+ password ;
+    return this._http.get(url);
+           };
+        
+
+  
+   
+
+}
