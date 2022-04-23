982cd77f4660ce0838e776bcd65c1c77e53d13a6
Jinhwa <jinhwa014@revature.net>
2022-04-23 18:48:27 -0500

    signout page fixed

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,6 @@
-// This is mainly for navbar menu showing depends on log in 
+// empty 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-import { Observable, BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators'
 
 
 
@@ -15,35 +12,13 @@ export class UserService {
 
 
 
-signedIn$ = new BehaviorSubject<boolean>(true)
-username: string = ''
-
-url = 'https://api.angular-email.com/auth' 
-
 constructor(private _http: HttpClient) {
 
  }
 
 
-signedIn(): Observable<SignedInResponse> {
-  return this._http.get<SignedInResponse>(`${this.url}/signedIn`).pipe(
-    tap((response: SignedInResponse)=> {
-      this.username = response.username
-      this.signedIn$.next(response.authenticated)
-    })
-  )
-}
-
-
-
-
-
 
 }
 
-interface SignedInResponse {
-  authenticated: boolean,
-  username: string
-}
 
 
